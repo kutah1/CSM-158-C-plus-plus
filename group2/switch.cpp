314ba32cca12d85c7ffe6f8afafb9cf802a2657a
kutah1 <jameskutah47@gmail.com>
2024-06-04 11:33:59 -0700

    changes

--- group2/switch.cpp
+++ group2/switch.cpp
@@ -11,20 +11,30 @@ int main()
     switch (grade)
         {
     case 'A':
+    case 'a':
         cout<<"Your grade is within the range of 70-100\n You have qualified to be interviewed";
             break;
     case 'B':
+    case 'b':
             cout<<"Your grade is within the range of 60-69\n You have qualified to be interviewed";
             break;
     case 'C':
+    case 'c':
+
             cout<<"Your grade is within the range of 50-59\n You have qualified to be interviewed";
             break;
     case 'D':
+    case 'd':
         cout <<"Your grade is within the range of 40-49\n You have qualified to be interviewed";
 
             break;
+            case 'F':
+            case 'f':
+            cout<<"Your grade is within the range of 30-39\n You have not qualified to be interviewed";
+            break;
         
-        default:
+        default:  
+        cout<<"Invalid grade output";
             break;
         }
  }
