7bd8e9983c927c8da033b34d09f7f9a2f0d9d03c
kutah1 <jameskutah47@gmail.com>
2024-06-11 11:30:10 -0700

    strangeconcepts

---
+++ group2/strangeconcept.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    if(0){
+        cout<<"Statement is true:"<<endl;
+    }
+    // the 0 indicates false and anything greater than 0  or a string is true.
+     cout<<"Statement is false"<<endl;
+      if(4){
+        cout<<"Statement is true:"<<endl;
+    }
+     cout<<"Statement is false"<<endl;
+     int age=10;
+      if(age=2){
+        cout<<"Statement is true:"<<endl;
+    }
+     cout<<"Statement is false"<<endl;
+      if("Good"){
+        cout<<"Statement is true:"<<endl;
+    }
+     cout<<"Statement is false"<<endl;
+}
