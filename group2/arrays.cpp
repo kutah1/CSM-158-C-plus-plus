fd81c660f790ebf18383e6cba149bec92bfb553a
kutah1 <jameskutah47@gmail.com>
2024-06-11 11:39:58 -0700

    changes

--- group2/arrays.cpp
+++ group2/arrays.cpp
@@ -1,6 +1,9 @@
 //Ways of declaring an array datatype arrayname[]={1,2,3,4}
 //datatype arrayname[n];
-// datatype[n][m];Arrays inside array. At every point an element is, there is another array
+// datatype[n][m];Arrays inside array. At every point an element is, there is another array 
+// n and m are the sizes of the arrays
+// datatype arrayname[5][5]={{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5}}
+// One dimensional array with multiple columns
 #include <iostream>
 using namespace std;
 int main(){
