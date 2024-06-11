47fddc7b730681a35f7afd85fa8316a7ae5c603a
kutah1 <jameskutah47@gmail.com>
2024-06-11 11:53:54 -0700

    changes

--- group2/arrays.cpp
+++ group2/arrays.cpp
@@ -4,9 +4,15 @@
 // n and m are the sizes of the arrays
 // datatype arrayname[5][5]={{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5}}
 // One dimensional array with multiple columns
+//to get 5
+//the first row is indicated by 0 and 
+//arrayname[0][4] prints 5
 #include <iostream>
 using namespace std;
 int main(){
-
+int arrayname[]={1,2,3,4,5};
+cout<<arrayname[3]<<endl;
+int arrayName[5][5]={{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5}};
+cout<<arrayName[0][4]<<endl;
     return 0;
 }
