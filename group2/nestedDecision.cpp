b92521b10c19389a55d35e2f6b513d259b6ba326
kutah1 <jameskutah47@gmail.com>
2024-06-11 11:17:47 -0700

    Nested ifs

---
+++ group2/nestedDecision.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+string shortcode;
+int option1;
+int option2;
+int main(){
+    cout<<"Welcome to Mini Momo..!"<<endl;
+    cout<<"Please enter your shorcode";
+    cin>>shortcode;
+    if(shortcode=="*170#"){
+cout<<"1.Transfer Money \n 2. Momo and Pay Bill \n 3.Airtime and Bundles \n 4. Allow Cash Out ";
+    }
+cin>>option1;
+
+
+return 0;    
+}
